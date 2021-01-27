354d2e2f88a66d09d2b8ff2de0d31fcfdf641f6a
Abdullatif Atci <latif.800@gmail.com>
2021-01-27 16:30:27 +0300

    Initial Commit

---
+++ News/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by Latif Atci on 1/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
