8c55de892cec624839d19f64ebb917e6fa847d54
Abdullatif Atci <latif.800@gmail.com>
2021-01-29 20:26:05 +0300

    Move Core Data Stack from AppDelegate to PersistenceManager

--- News/Persistence/PersistanceManager.swift
+++ News/Persistence/PersistanceManager.swift
@@ -10,12 +10,10 @@ import CoreData
 
 final class PersistanceManager: PersistanceContracts {
     typealias T = Article
-    let appDelegate = UIApplication.shared.delegate as! AppDelegate
-    
-    
+        
     func save(data: Article) {
         let managedContext =
-          appDelegate.persistentContainer.viewContext
+          persistentContainer.viewContext
         let entity =
           NSEntityDescription.entity(forEntityName: "Favorites",
                                      in: managedContext)!
@@ -36,8 +34,8 @@ final class PersistanceManager: PersistanceContracts {
         let request = NSBatchDeleteRequest(fetchRequest: fetchRequest)
         
         do {
-            try appDelegate.persistentContainer.viewContext.execute(request)
-            appDelegate.saveContext()
+            try persistentContainer.viewContext.execute(request)
+            saveContext()
         } catch {
             failed(.removingError)
         }
@@ -52,7 +50,7 @@ final class PersistanceManager: PersistanceContracts {
         fetchRequest.predicate = NSPredicate(format: "title == %@", title)
         
         do {
-            let booksCount = try appDelegate.persistentContainer.viewContext.count(for: fetchRequest)
+            let booksCount = try persistentContainer.viewContext.count(for: fetchRequest)
             if booksCount > 0 {
                 completion(.success(true))
             } else {
@@ -60,6 +58,51 @@ final class PersistanceManager: PersistanceContracts {
             }
         } catch {
             completion(.failure(.checkingError))
+        }
+    }
+    
+    // MARK: - Core Data stack
+
+    private lazy var persistentContainer: NSPersistentContainer = {
+        /*
+         The persistent container for the application. This implementation
+         creates and returns a container, having loaded the store for the
+         application to it. This property is optional since there are legitimate
+         error conditions that could cause the creation of the store to fail.
+        */
+        let container = NSPersistentContainer(name: "News")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                 
+                /*
+                 Typical reasons for an error here include:
+                 * The parent directory does not exist, cannot be created, or disallows writing.
+                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
+                 * The device is out of space.
+                 * The store could not be migrated to the current model version.
+                 Check the error message to determine what the actual problem was.
+                 */
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+
+    // MARK: - Core Data Saving support
+
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
         }
     }
     
